7ade3b6ced5d7fadddba9ebcba7ccda41b1b1e7b
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-20 19:00:16 +0200

    Before ending build all

--- _sunamo/StringExtensions.cs
+++ _sunamo/StringExtensions.cs
@@ -1,5 +1,3 @@
-
-
 namespace
 #if SunamoFubuCsProjFile
 SunamoFubuCsProjFile
@@ -7,8 +5,6 @@ SunamoFubuCsProjFile
 SunamoFubuCore
 #endif
 ;
-
-
 public static class StringExtensions
 {
     /// <summary>
@@ -21,20 +17,16 @@ public static class StringExtensions
     public static string CombineToPath(this string path, string root)
     {
         if (Path.IsPathRooted(path)) return path;
-
         return Path.Combine(root, path);
     }
-
     public static void IfNotNull(this string target, Action<string> continuation)
     {
         if (target != null) continuation(target);
     }
-
     public static string ToFullPath(this string path)
     {
         return Path.GetFullPath(path);
     }
-
     /// <summary>
     ///     Retrieve the parent directory of a directory or file
     ///     Shortcut to Path.GetDirectoryName(path)
@@ -45,7 +37,6 @@ public static class StringExtensions
     {
         return Path.GetDirectoryName(path.TrimEnd(Path.DirectorySeparatorChar));
     }
-
     /// <summary>
     ///     Equivalent of FileSystem.Combine( [Union of path, parts] )
     /// </summary>
@@ -58,16 +49,13 @@ public static class StringExtensions
 {
 path
 };
-
         list.AddRange(parts);
         return FileSystem.Combine(list.ToArray());
     }
-
     public static string PathRelativeTo(this string path, string root)
     {
         var pathParts = path.getPathParts();
         var rootParts = root.getPathParts();
-
         var length = pathParts.Count > rootParts.Count ? rootParts.Count : pathParts.Count;
         for (var i = 0; i < length; i++)
             if (pathParts.First().Equals(rootParts.First(), StringComparison.OrdinalIgnoreCase))
@@ -79,40 +67,31 @@ path
             {
                 break;
             }
-
         for (var i = 0; i < rootParts.Count; i++) pathParts.Insert(0, "..");
-
         return pathParts.Count > 0 ? FileSystem.Combine(pathParts.ToArray()) : string.Empty;
     }
-
     public static bool IsEmpty(this string stringValue)
     {
         return string.IsNullOrEmpty(stringValue);
     }
-
     public static bool IsNotEmpty(this string stringValue)
     {
         return !string.IsNullOrEmpty(stringValue);
     }
-
     public static void IsNotEmpty(this string stringValue, Action<string> action)
     {
         if (stringValue.IsNotEmpty())
             action(stringValue);
     }
-
     public static bool ToBool(this string stringValue)
     {
         if (string.IsNullOrEmpty(stringValue)) return false;
-
         return bool.Parse(stringValue);
     }
-
     public static string ToFormat(this string stringFormat, params Object[] args)
     {
         return string.Format(stringFormat, args);
     }
-
     /// <summary>
     ///     Performs a case-insensitive comparison of strings
     /// </summary>
@@ -120,7 +99,6 @@ path
     {
         return thisString.Equals(otherString, StringComparison.InvariantCultureIgnoreCase);
     }
-
     /// <summary>
     ///     Converts the string to Title Case
     /// </summary>
@@ -128,7 +106,6 @@ path
     {
         return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(stringValue);
     }
-
     /// <summary>
     ///     Formats a multi-line string for display on the web
     /// </summary>
@@ -137,7 +114,6 @@ path
     {
         return new Regex("(\r\n|\n)").Replace(plainText, "<br/>");
     }
-
     /// <summary>
     ///     Returns a DateTime value parsed from the <paramref name="dateTimeValue" /> parameter.
     /// </summary>
@@ -147,30 +123,24 @@ path
     {
         return DateTime.Parse(dateTimeValue);
     }
-
     public static string ToGmtFormattedDate(this DateTime date)
     {
         return date.ToString("yyyy'-'MM'-'dd hh':'mm':'ss tt 'GMT'");
     }
-
     public static string[] ToDelimitedArray(this string content)
     {
         return content.ToDelimitedArray(',');
     }
-
     public static string[] ToDelimitedArray(this string content, char delimiter)
     {
         var array = content.Split(delimiter);
         for (var i = 0; i < array.Length; i++) array[i] = array[i].Trim();
-
         return array;
     }
-
     public static bool IsValidNumber(this string number)
     {
         return IsValidNumber(number, Thread.CurrentThread.CurrentCulture);
     }
-
     public static bool IsValidNumber(this string number, CultureInfo culture)
     {
         var _validNumberPattern =
@@ -179,20 +149,16 @@ path
         @"\d{3})+)?(?:\"
         + culture.NumberFormat.NumberDecimalSeparator +
         @"\d+)?$";
-
         return new Regex(_validNumberPattern, RegexOptions.ECMAScript).IsMatch(number);
     }
-
     public static IList<string> getPathParts(this string path)
     {
         return path.Split(new[] { Path.DirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries).ToList();
     }
-
     public static string DirectoryPath(this string path)
     {
         return Path.GetDirectoryName(path);
     }
-
     /// <summary>
     ///     Reads text and returns an enumerable of strings for each line
     /// </summary>
@@ -204,7 +170,6 @@ path
         string line;
         while ((line = reader.ReadLine()) != null) yield return line;
     }
-
     /// <summary>
     ///     Reads text and calls back for each line of text
     /// </summary>
@@ -216,7 +181,6 @@ path
         string line;
         while ((line = reader.ReadLine()) != null) callback(line);
     }
-
     /// <summary>
     ///     Just uses MD5 to create a repeatable hash
     /// </summary>
@@ -226,7 +190,6 @@ path
     {
         return MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(text)).Select(b => b.ToString("x2")).Join("");
     }
-
     /// <summary>
     ///     Splits a camel cased string into seperate words delimitted by a space
     /// </summary>
@@ -236,7 +199,6 @@ path
     {
         return Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
     }
-
     /// <summary>
     ///     Splits a pascal cased string into seperate words delimitted by a space
     /// </summary>
@@ -246,14 +208,12 @@ path
     {
         return str.SplitCamelCase();
     }
-
     public static TEnum ToEnum<TEnum>(this string text) where TEnum : struct
     {
         var enumType = typeof(TEnum);
         if (!enumType.IsEnum) throw new ArgumentException("{0} is not an Enum".ToFormat(enumType.Name));
         return (TEnum)Enum.Parse(enumType, text, true);
     }
-
     /// <summary>
     ///     Wraps a string with parantheses.  Originally used to file escape file names when making command line calls
     /// </summary>
